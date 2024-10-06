536850d524224112bda15ad0ba0f446918e3c086
Carlos Uribe <carlosuribe0323@gmail.com>
2024-10-06 03:22:42 -0400

    rrrr

--- Client/src/components/footer/FooterAdmin.js
+++ Client/src/components/footer/FooterAdmin.js
@@ -1,98 +1,107 @@
-/*eslint-disable*/
+/* eslint-disable */
 import React from "react";
 import {
   Flex,
   Link,
-  List,
-  ListItem,
   Text,
   Button,
+  IconButton,
   useColorMode,
   useColorModeValue,
+  HStack,
 } from "@chakra-ui/react";
+import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 export default function Footer() {
   const textColor = useColorModeValue("blue.700", "white");
   const { toggleColorMode } = useColorMode();
+
   return (
     <Flex
-      zIndex='3'
+      zIndex="3"
       flexDirection={{
         base: "column",
         xl: "row",
       }}
-      alignItems={{
-        base: "center",
-        xl: "start",
-      }}
-      justifyContent='space-between'
+      alignItems="center"
+      justifyContent="space-between"
       px={{ base: "30px", md: "50px" }}
-      pb='30px'>
+      py="30px"
+      bg={useColorModeValue("gray.100", "gray.900")}
+    >
       <Text
         color={textColor}
-        textAlign={{
-          base: "center",
-          xl: "start",
-        }}
-        mb={{ base: "20px", xl: "0px" }}>
-        {" "}
-        &copy; {1900 + new Date().getYear()}
-        <Text as='span' fontWeight='500' ms='4px'>
+        textAlign="center"
+        mb={{ base: "20px", xl: "0px" }}
+      >
+        &copy; {new Date().getFullYear()}{" "}
+        <Text as="span" fontWeight="500" ms="4px">
           <Link
-            fontWeight='500'
+            fontWeight="500"
             color={textColor}
             target="_blank"
-            href='https://prolinkinfotech.com/'> Prolink Infotech.</Link> Need help, facing issues, or looking for a new feature? Contact us for paid support and services at <Link href='mailto:alternatecrm@gmail.com' fontWeight='500'
-              color={textColor}
-              target="_blank">alternatecrm@gmail.com</Link>
+            href="https://prolinkinfotech.com/"
+          >
+            Atlas.
+          </Link>
+          {" "}Need help or facing issues? Reach us at{" "}
+          <Link
+            href="mailto:alternatecrm@gmail.com"
+            fontWeight="500"
+            color={textColor}
+            target="_blank"
+          >
+            alternatecrm@gmail.com
+          </Link>
         </Text>
       </Text>
-      {/* <List display='flex'>
-        <ListItem
-          me={{
-            base: "20px",
-            md: "44px",
-          }}>
-          <Link
-            fontWeight='500'
-            color={textColor}
-            href='https://prolinkinfotech.com/'>
-            Support
-          </Link>
-        </ListItem>
-        <ListItem
-          me={{
-            base: "20px",
-            md: "44px",
-          }}>
-          <Link
-            fontWeight='500'
-            color={textColor}
-            href='https://www.simmmple.com/licenses?ref=horizon-chakra-free'>
-            License
-          </Link>
-        </ListItem>
-        <ListItem
-          me={{
-            base: "20px",
-            md: "44px",
-          }}>
-          <Link
-            fontWeight='500'
-            color={textColor}
-            href='https://simmmple.com/terms-of-service?ref=horizon-chakra-free'>
-            Terms of Use
-          </Link>
-        </ListItem>
-        <ListItem>
-          <Link
-            fontWeight='500'
-            color={textColor}
-            href='https://www.blog.simmmple.com/?ref=horizon-chakra-free'>
-            Blog
-          </Link>
-        </ListItem>
-      </List> */}
+
+      <HStack spacing="20px">
+        <IconButton
+          as={Link}
+          href="#"
+          aria-label="Facebook"
+          icon={<FaFacebook />}
+          variant="ghost"
+          color={textColor}
+          _hover={{ color: "blue.500" }}
+        />
+        <IconButton
+          as={Link}
+          href="#"
+          aria-label="Twitter"
+          icon={<FaTwitter />}
+          variant="ghost"
+          color={textColor}
+          _hover={{ color: "blue.400" }}
+        />
+        <IconButton
+          as={Link}
+          href="#"
+          aria-label="LinkedIn"
+          icon={<FaLinkedin />}
+          variant="ghost"
+          color={textColor}
+          _hover={{ color: "blue.600" }}
+        />
+        <IconButton
+          as={Link}
+          href="#"
+          aria-label="Instagram"
+          icon={<FaInstagram />}
+          variant="ghost"
+          color={textColor}
+          _hover={{ color: "pink.400" }}
+        />
+      </HStack>
+
+      <Button
+        size="sm"
+        mt={{ base: "20px", xl: "0px" }}
+        onClick={toggleColorMode}
+      >
+        Toggle {useColorModeValue("Dark", "Light")} Mode
+      </Button>
     </Flex>
   );
 }
